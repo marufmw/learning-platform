2ea4f80f4f928f649a780e39a377fa4de165b34a
maruf <mdmarufbinsalim@gmail.com>
2026-04-16 19:46:13 +0600

    feat: added clerk sign in and signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+
+
+function Logo() {
+  return (
+    <Link href="/">
+      <Link href="/">
+        <svg
+          className="absolute left-2 top-2 h-8 w-8"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+      </Link>
+    </Link>
+  );
+}
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,6 +37,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex items-center justify-center min-h-screen">
+      <Logo />
       {children}
     </div>
   );
